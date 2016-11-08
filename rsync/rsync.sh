598e0d3523848884ec158900b44cd59dc89f3ef7
t-kiyotaki <t-kiyotaki@bfp.co.jp>
2016-11-09 01:18:38 +0900

    comment update

--- rsync/rsync.sh
+++ rsync/rsync.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
 #リモートからローカルにダウンロード
 #rsync [options] SOURCE DESTINATION
+
 rsync -auvz --delete -e 'ssh -p [port]' [id]@[host]:[source_path] [dest_path]
+
+#example
+#rsync -auvz --delete -e 'ssh -p xxxxxx' root@example.com:/var/www/path/to/dir/ dir/
